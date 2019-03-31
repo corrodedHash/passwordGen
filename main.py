1299ea15689b92b0fae4a146be06c73fe7307c94
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-03-31 23:56:12 +0200

    Linting

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import urllib.request
 import os
 
 
-def rollDice(amount: int) -> List[int]:
+def roll_dice(amount: int) -> List[int]:
+    """Rolls amount six sided dice"""
     result: List[int] = []
-    for i in range(amount):
+    for _ in range(amount):
         result.append(secrets.randbelow(6) + 1)
     return result
 
 
 @lru_cache(1)
-def getWordlist():
+def get_wordlist():
+    """Create word list"""
     WORDFILE = "./diceware.wordlist.asc"
     WORDFILEURL = "http://world.std.com/~reinhold/diceware.wordlist.asc"
     WORDFILEHASH = "3cd6164a99e95381f8620aec782a933545bcd5833fa331d267a6829f6665256e"
@@ -26,37 +28,39 @@ def getWordlist():
     with open(WORDFILE, "r", encoding="utf-8") as myfile:
         data = myfile.read()
 
-    dataList = data.split("\n")
-    dataList = dataList[2:]
-    dataList = dataList[:-12]
+    data_list = data.split("\n")
+    data_list = data_list[2:]
+    data_list = data_list[:-12]
 
     #ideally would check pgp signature here
-    assert(hashlib.sha256(data.encode("utf-8")).hexdigest() == WORDFILEHASH)
-    assert(dataList[0][:5] == "11111")
-    assert(dataList[-1][:5] == "66666")
+    assert hashlib.sha256(data.encode("utf-8")).hexdigest() == WORDFILEHASH
+    assert data_list[0][:5] == "11111"
+    assert data_list[-1][:5] == "66666"
 
-    return dataList
+    return data_list
 
 
-def getWord():
-    data: List[str] = getWordlist()
+def get_word():
+    """Create word"""
+    data: List[str] = get_wordlist()
 
-    diceStr = "".join(map(str, rollDice(5)))
+    dice_str = "".join(map(str, roll_dice(5)))
 
     for s in data:
-        if diceStr == s[:5]:
+        if dice_str == s[:5]:
             return s[6:-1]
 
-    print(diceStr)
-    raise Exception(diceStr + " not found in file")
+    print(dice_str)
+    raise Exception(dice_str + " not found in file")
 
 
-def getPassword(wordAmount: int = 4):
+def get_password(word_amount: int = 4):
+    """Create password"""
     result = ""
-    for i in range(wordAmount):
-        word = getWord().capitalize()
+    for _ in range(word_amount):
+        word = get_word().capitalize()
         result += word
     return result + str(secrets.randbelow(100))
 
 
-print(getPassword())
+print(get_password())
